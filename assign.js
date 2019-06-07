f5beccaf5dd8811bb3bf6fcebb872d1639ee13ef
bhavesh.zenatix <bhavesh.pant@zenatix.com>
2019-06-07 08:38:27 +0530

    svg - pythogras

---
+++ assign.js
@@ -0,0 +1,217 @@
+// todo: add constraint params
+function makeDraggable(evt) {
+    var points = ['P1', 'P2', 'P3', 'P4'];
+    var lines = ['L1', 'L2', 'L3', 'L4'];
+    var svg = evt.target;
+    // console.log('mouse position')
+    svg.addEventListener('mousedown', startDrag);
+    svg.addEventListener('mousemove', drag);
+    svg.addEventListener('mouseup', endDrag);
+    svg.addEventListener('mouseleave', endDrag);
+    svg.addEventListener('touchstart', startDrag);
+    svg.addEventListener('touchmove', drag);
+    svg.addEventListener('touchend', endDrag);
+    svg.addEventListener('touchleave', endDrag);
+    svg.addEventListener('touchcancel', endDrag);
+
+    var selectedPointElement, offset, transform, cx, cy,
+        minX, maxX, minY, maxY, ID, highlighter1, highlighter2;
+
+
+    function getMousePosition(evt) {
+        var CTM = svg.getScreenCTM();
+        if (evt.touches) { evt = evt.touches[0]; }
+        return {
+            x: (evt.clientX - CTM.e) / CTM.a,
+            y: (evt.clientY - CTM.f) / CTM.d
+        };
+    }
+
+    function setLineCoordinates(lineElement, { x1, y1, x2, y2 }) {
+        lineElement.setAttribute('x1', x1)
+        lineElement.setAttribute('y1', y1)
+        lineElement.setAttribute('x2', x2)
+        lineElement.setAttribute('y2', y2)
+
+    }
+
+    function setHiglighterCoordinates() {
+        if (ID === 'P1' || ID === 'P3') {
+            setLineCoordinates(highlighter1, {
+                x1: cx <= 60 ? 0 : 300,
+                y1: cy,
+                x2: cx + transform.matrix.e,
+                y2: cy
+            })
+            setLineCoordinates(highlighter2, {
+                x1: cx <= 60 ? 300 : 0,
+                y1: cy,
+                x2: cx + transform.matrix.e,
+                y2: cy
+            })
+        }
+        else {
+            setLineCoordinates(highlighter1, {
+                x1: cx,
+                y1: cy <= 60 ? 0 : 300,
+                x2: cx,
+                y2: cy + transform.matrix.f
+            })
+            setLineCoordinates(highlighter2, {
+                x1: cx,
+                y1: cx <= 60 ? 300 : 0,
+                x2: cx,
+                y2: cy + transform.matrix.f
+            })
+        }
+    }
+
+    function appendHighlighters() {
+        // initialize higlighters
+        highlighter1 = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+        highlighter2 = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+
+        highlighter1.setAttribute('stroke', 'green');
+        highlighter1.setAttribute('id', 'Highlighter1');
+        highlighter1.setAttribute('stroke-width', 4);
+        highlighter1.setAttribute('stroke-linecap', 'round');
+
+        highlighter2.setAttribute('stroke', 'dodgerblue');
+        highlighter2.setAttribute('id', 'Highlighter2');
+        highlighter2.setAttribute('stroke-width', 4);
+        highlighter2.setAttribute('stroke-linecap', 'round');
+
+        setHiglighterCoordinates();
+
+        // append higlighter
+        selectedPointElement.parentNode.insertBefore(highlighter1, selectedPointElement);
+        selectedPointElement.parentNode.insertBefore(highlighter2, selectedPointElement)        
+    }
+
+
+    function removeHighlighters() {
+        highlighter1.parentNode.removeChild(highlighter1);
+        highlighter2.parentNode.removeChild(highlighter2);
+        highlighter1 = false;
+        highlighter2 = false;
+    }
+
+    function moveLines() {
+        for (let line of lines) {
+            let lineElement = document.getElementById(line)
+            let lineNumber = Number(line[1]); //cx.baseVal.value
+            let point1Element = document.getElementById(`P${lineNumber}`)
+            let point2Element = document.getElementById(`P${lineNumber === 4 ? 1 : lineNumber + 1}`)
+
+            let point1Matrix = point1Element.transform.baseVal.getItem(0).matrix;
+            let point2Matrix = point2Element.transform.baseVal.getItem(0).matrix;
+
+            setLineCoordinates(lineElement, {
+                x1: point1Element.cx.baseVal.value + point1Matrix.e,
+                y1: point1Element.cy.baseVal.value + point1Matrix.f,
+                x2: point2Element.cx.baseVal.value + point2Matrix.e,
+                y2: point2Element.cy.baseVal.value + point2Matrix.f
+            })
+            // lineElement.setAttribute('x1', point1Element.cx.baseVal.value + point1Matrix.e)
+            // lineElement.setAttribute('y1', point1Element.cy.baseVal.value + point1Matrix.f)
+            // lineElement.setAttribute('x2', point2Element.cx.baseVal.value + point2Matrix.e)
+            // lineElement.setAttribute('y2', point2Element.cy.baseVal.value + point2Matrix.f)
+
+        }
+    }
+
+    function moveOtherPointers(delta) {
+
+        for (let point of points) {
+            if (point !== ID) {
+                let pointElement = document.getElementById(point);
+                let shiftDelta = ID[1] === "3" || ID[1] === "4" ? -delta : delta;
+
+                if (point === 'P1') {
+                    pointElement.setAttribute('transform', `translate(${shiftDelta},${0})`);
+                }
+                else if (point === 'P2') {
+                    pointElement.setAttribute('transform', `translate(${0},${shiftDelta})`);
+                }
+                else if (point === 'P3') {
+                    pointElement.setAttribute('transform', `translate(${-shiftDelta},${0})`);
+                }
+                else {
+                    pointElement.setAttribute('transform', `translate(${0},${-shiftDelta})`);
+                }
+            }
+        }
+        moveLines()
+    }
+
+    function startDrag(evt) {
+        if (evt.target.classList.contains('draggable')) {
+            selectedPointElement = evt.target;
+            offset = getMousePosition(evt);
+
+            var transforms = selectedPointElement.transform.baseVal;
+
+            if (transforms.length === 0 || transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
+                // Create an transform that translates by (0, 0)
+                var translate = svg.createSVGTransform();
+                translate.setTranslate(0, 0);
+                selectedPointElement.transform.baseVal.insertItemBefore(translate, 0);
+            }
+
+            // Get initial translation
+            transform = transforms.getItem(0);
+            offset.x -= transform.matrix.e;
+            offset.y -= transform.matrix.f;
+
+            ID = selectedPointElement.id;
+            cx = selectedPointElement.cx.baseVal.value;
+            cy = selectedPointElement.cy.baseVal.value;
+
+            if (ID === 'P1' || ID === 'P3') {
+                minX = -cx
+                maxX = 300 - cx
+                minY = 0
+                maxY = 0
+            }
+
+            // for (ID === 'P4' || ID === 'P2')
+            else {
+                minX = 0
+                maxX = 0
+                minY = -cy
+                maxY = 300 - cy
+            }
+            console.log('heree offset', offset, transform, { cx, cy })
+            appendHighlighters()
+        }
+    }
+
+    function drag(evt) {
+        if (selectedPointElement) {
+            evt.preventDefault();
+
+            var coord = getMousePosition(evt);
+            var dx = coord.x - offset.x;
+            var dy = coord.y - offset.y;
+            var delta
+
+            if (dx < minX) { dx = minX; }
+            else if (dx > maxX) { dx = maxX; }
+            if (dy < minY) { dy = minY; }
+            else if (dy > maxY) { dy = maxY; }
+            delta = dx || dy;
+            console.log('dxx', dx, dy)
+            selectedPointElement.setAttribute('opacity', 1)
+            // move other points, lines, highlight
+            setHiglighterCoordinates();
+            moveOtherPointers(delta)
+            transform.setTranslate(dx, dy);
+        }
+    }
+
+    function endDrag(evt) {
+        if (highlighter1 || highlighter2)
+            removeHighlighters();
+        selectedPointElement = false;
+    }
+}
---
+++ main.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+    document.getElementById("text").addEventListener("click", generateSquare);
+
+    let squareSVG = document.getElementById("squareSVG");
+    let svgDoc = squareSVG.contentDocument;
+    let timeOut;
+
+    function createSquare() {
+        let points = ['P1', 'P2', 'P3', 'P4'];
+        var polygonPoints = "";
+
+        for (let point of points) {
+            let pointElement = svgDoc.getElementById(point);
+            let pointMatrix = pointElement.transform.baseVal.length === 0 ? { e: 0, f: 0 } : pointElement.transform.baseVal.getItem(0).matrix;
+            polygonPoints += `${pointElement.cx.baseVal.value + pointMatrix.e},${pointElement.cy.baseVal.value + pointMatrix.f} `;
+        }
+
+        // create and append polygon
+        const squareClone = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
+
+        // calculate the angle to align the square
+        let point1Delta = svgDoc.getElementById('P1').transform.baseVal.length === 0 ? 0 : svgDoc.getElementById('P1').transform.baseVal.getItem(0).matrix.e;
+        const rotateAngle = Math.atan(Math.abs(point1Delta + 60) / Math.abs(point1Delta - 240))
+
+        squareClone.setAttribute('points', polygonPoints);
+        squareClone.setAttribute('id', 'square-clone');
+        squareClone.setAttribute('fill', 'dodgerblue');
+        squareClone.animate([
+            // keyframes
+            { transform: 'translate(0px, 0px) rotate(0deg)' },
+            { transform: `translate(350px) rotate(${rotateAngle}rad)` }
+        ], {
+                duration: 800,
+                iterations: 1,
+                fill: 'forwards'
+            });
+
+        svgDoc.getElementById('square').append(squareClone)
+    }
+
+    function generateSquare() {
+        if (!svgDoc.getElementById('square-clone')) {
+            createSquare();
+            timeOut = setTimeout(function () {
+                let elem = svgDoc.getElementById('square-clone');
+                return elem.parentNode.removeChild(elem);
+            }, 6000)
+        }
+        // else {
+        //     clearTimeout(timeOut);
+        //     let elem = svgDoc.getElementById('square-clone');
+        //     elem.parentNode.removeChild(elem);
+        //     createSquare();
+        // }
+    }
+}
